c8d8744d94297ccd432f2dd574fd1c5dab8c436b
aman7github <amanmaurya9044@gmail.com>
2025-05-02 18:51:09 +0530

    remove express env

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
-const express = require('express')
-const fs = require('fs')
-require('dotenv').config()
-const app= express()
 
-app.get('/getTimeStories',(req,res)=>{
+const http = require('http');
+const fs = require('fs')
+
+
+
+const server = http.createServer((req,res)=>{
+
+ if(req.url==='/getTimeStories' && req.method==="GET"){
 
   fs.readFile('./db.json',"utf-8",(err,data)=>{
     
     if(data){
         try{
           let stories = JSON.parse(data)
-          res.status(200).send(stories)
+      
+          res.end(JSON.stringify(stories))
         }catch(err){
-             res.status(400).send({"err":err})
+             res.end(JSON.stringify({"err":err}))
         }
 
     }else{
-       res.status(400).send({"err":err})
+       res.end(JSON.stringify({"err":err}))
     }
 
   })
 
+
+ }else{
+  res.end(JSON.stringify({"err":"wrong url or wrong method"}))
+
+ }
+
+
 })
  
-app.listen(process.env.PORT,()=>{
+
+
+server.listen(3000,()=>{
     console.log('server is connected');
     
 })
